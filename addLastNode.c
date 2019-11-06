4e410e8e34795dc9b7b790f0eb386eb7abd45f69
KCH6937 <adasddasd12@naver.com>
2019-11-06 17:25:14 +0900

    source

--- addLastNode.c
+++ addLastNode.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
 
 struct NODE {
 	struct NODE *next;
 	int dataX;
 	int dataY;
 };
-
-int cnt = 1;
 
 void addNode(int dataX, int dataY)
 {
@@ -17,31 +14,25 @@ void addNode(int dataX, int dataY)
 	newNode->dataY = dataY;
 	newNode->next = NULL;
 	
-	while(newNode == NULL) 
+	while(newNode == NULL)
 	{
 		struct NODE *newNode2 = malloc(sizeof(struct NODE));
 		newNode->next = newNode2;
 		newNode = newNode->next;
 	}
 	
-	while (newNode != NULL) 
+	while (newNode != NULL)
 	{
 		printf("%d 	%d\n", newNode->dataX, newNode->dataY);
 		newNode = newNode->next;
 	}
-	
-
 }
 
 int main(void)
 {
-	
 	addNode(10,20);
 	addNode(30,40);
 	addNode(50,60);
-	addNode(70,80);
-
-	
 
 	return 0;	
 }
